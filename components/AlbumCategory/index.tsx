93bf9808f976e77b7e33290a7fd92c4e68ca2754
chr-ge <ace26@protonmail.com>
2021-02-03 20:52:49 -0500

    feat: create AlbumCategory component

---
+++ components/AlbumCategory/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, Text, FlatList } from "react-native";
+import { Album } from "../../types";
+import AlbumCard from '../AlbumCard'
+import styles from "./styles";
+
+interface AlbumCategoryPropsInterface {
+  title: string;
+  albums: Album[];
+}
+
+const AlbumCategory = ({ title, albums }: AlbumCategoryPropsInterface) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+      <FlatList
+        data={albums}
+        renderItem={({ item }) => <AlbumCard id={item.id} imageUri={item.imageUri} artistHeadline={item.artistHeadline} />}
+        keyExtractor={(item) => item.id}
+        showsHorizontalScrollIndicator={false}
+        horizontal
+      />
+    </View>
+  );
+};
+
+export default AlbumCategory;
