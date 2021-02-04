41762dc0cab05038189ca4872780a90b1b624b19
chr-ge <ace26@protonmail.com>
2021-02-03 20:52:31 -0500

    feat: create AlbumCard component

---
+++ components/AlbumCard/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, Image, Text, TouchableWithoutFeedback } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Album } from '../../types'
+import styles from './styles'
+
+const AlbumCard = ({ id, imageUri, artistHeadline }: Album) => {
+  const navigation = useNavigation()
+
+  return (
+    <TouchableWithoutFeedback onPress={() => navigation.navigate('AlbumScreen', { id })}>
+      <View style={styles.container}>
+          <Image source={{ uri: imageUri }} style={styles.image} />
+          <Text style={styles.headline}>{ artistHeadline }</Text>
+      </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+export default AlbumCard;
